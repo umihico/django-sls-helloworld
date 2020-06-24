ec475707787aaad236c1c835ea8a6d6869e29c16
umihico <umihico@users.noreply.github.com>
2020-06-24 09:57:24 +0900

    import django requirements with test

--- handler.py
+++ handler.py
@@ -1,7 +1,12 @@
 import json
+import requirements
 
 
 def hello(event, context):
+    # testing import libraries
+    import django
+    import werkzeug
+    import pymysql
     body = {
         "message": "Go Serverless v1.0! Your function executed successfully!",
         "input": event
